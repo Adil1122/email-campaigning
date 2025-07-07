abbbf76b069cf4fdf3ae537f8e9c9daf48dbcaf9
Adil1122 <adil7090@gmail.com>
2025-07-07 14:59:24 +0500

    migration / seeder changes

--- database/migrations/2025_06_24_150258_create_w_records_table.php
+++ database/migrations/2025_06_24_150258_create_w_records_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('w_records', function (Blueprint $table) {
+        Schema::create('whatsaapautomations', function (Blueprint $table) {
             $table->id();
             $table->string('keyword')->nullable();
             $table->string('business_name')->nullable();
